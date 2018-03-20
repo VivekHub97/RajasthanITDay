2e0f6950b349ab1907ce5bd1775cb497a4741afd
msdeepali <singhaldeepali96@gmail.com>
2018-03-20 23:08:21 +0530

    ok
    ok

---
+++ RajasthanHackathon/Assets/Scripts/IntroLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroLevel : MonoBehaviour {
+
+	
+    public void RotatedLevelClick()
+    {
+        SceneManager.LoadScene("rotatedPathsLevel");
+    }
+}
